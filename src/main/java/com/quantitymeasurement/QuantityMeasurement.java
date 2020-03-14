576be7ffaf6b3dbfc8c36d22c0bf8829922e5ba4
jitesh <jitesh0dubey@gmail.com>
2020-03-14 11:04:10 +0530

    ADDED : Test Case For When Feet Value Is Zero Should Return Equal

--- src/main/java/com/quantitymeasurement/QuantityMeasurement.java
+++ src/main/java/com/quantitymeasurement/QuantityMeasurement.java
@@ -1,7 +1,22 @@
 package com.quantitymeasurement;
 
+import java.util.Objects;
+
 public class QuantityMeasurement {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Quantity Management Problem");
+    double feetValue;
+
+    public QuantityMeasurement(double value) {
+        feetValue=value;
+    }
+
+    public QuantityMeasurement() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        QuantityMeasurement that = (QuantityMeasurement) o;
+        return Double.compare(that.feetValue, feetValue) == 0;
     }
 }
--- src/test/java/com/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/quantitymeasurement/QuantityMeasurementTest.java
@@ -1,5 +1,27 @@
 package com.quantitymeasurement;
 
-public class QuantityMeasurementTest {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
+public class QuantityMeasurementTest {
+    QuantityMeasurement measurement;
+
+    @Before
+    public void setUp() throws Exception {
+        measurement=new QuantityMeasurement();
+    }
+
+    @Test
+    public void givenFeetValueZero_WhenEqual_ShouldReturnTrue() {
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement(0);
+        boolean equalCheck = quantityMeasurement.equals(measurement);
+        Assert.assertTrue(equalCheck);
+    }
+    @Test
+    public void givenFeetValueZero_WhenNotEqual_ShouldReturnFalse() {
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement(12);
+        boolean equalCheck = quantityMeasurement.equals(measurement);
+        Assert.assertFalse(equalCheck);
+    }
 }
