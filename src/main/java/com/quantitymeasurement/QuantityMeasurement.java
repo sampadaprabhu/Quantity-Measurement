fc20a5ec95be7634a4a9bbaa5947618604b90436
jitesh <jitesh0dubey@gmail.com>
2020-03-14 09:47:37 +0530

    ADDED:Welcome Message

---
+++ src/main/java/com/quantitymeasurement/QuantityMeasurement.java
@@ -0,0 +1,7 @@
+package com.quantitymeasurement;
+
+public class QuantityMeasurement {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Quantity Management Problem");
+    }
+}
---
+++ src/test/java/com/quantitymeasurement/QuantityMeasurementTest.java
@@ -0,0 +1,5 @@
+package com.quantitymeasurement;
+
+public class QuantityMeasurementTest {
+
+}
